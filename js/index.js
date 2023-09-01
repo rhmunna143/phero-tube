73d2e19f088fece38dfed8312a4d52f29a3cc76b
rhmunna143 <rhmunna19@gmail.com>
2023-09-01 11:48:35 +0600

    cat api fetched

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
+const allCategories = async() => {
+    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
+    const data = await response.json();
 
+    console.log("test", data.data);
+}
 
-
+allCategories();
 
 
 
